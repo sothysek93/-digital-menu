58dd8d1ebae728a2060478200bb12ad518cfcfbf
Sothy SEK <sothysek93@gmail.com>
2026-04-02 16:08:40 +0700

    fix build: implement stable dist folder bridge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   nitro: {
-    preset: 'cloudflare-pages'
+    preset: 'cloudflare-pages',
+    output: {
+      dir: '.output',
+      serverDir: '.output/server',
+      publicDir: '.output/public'
+    }
   },
   css: ['~/assets/css/main.css'],
   modules: ['shadcn-nuxt'],
